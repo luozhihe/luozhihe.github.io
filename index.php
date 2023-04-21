b44fafb37b2a78e6470de95fb4a75aafda92bfc1
luozhihe <120428921+luozhihe@users.noreply.github.com>
2023-04-21 20:17:33 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>My Blog</title>
+</head>
+<body>
+	<header>
+		<h1>My Blog</h1>
+	</header>
+
+	<nav>
+		<ul>
+			<li><a href="index.php">Home</a></li>
+			<li><a href="blog.php">Blog</a></li>
+			<li><a href="contact.php">Contact</a></li>
+		</ul>
+	</nav>
+
+	<main>
+		<h2>Welcome to my blog</h2>
+		<p>Here you can read my latest posts and leave comments.</p>
+	</main>
+
+	<footer>
+		<p>&copy; 2023 My Blog</p>
+	</footer>
+</body>
+</html>
